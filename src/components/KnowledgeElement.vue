7a81e689edb1f35fbf254f30514033bead704223
ViktarStar <v.starovoitou@belvg.com>
2023-05-14 13:44:27 +0200

    Раздел 8: Component Communication

---
+++ src/components/KnowledgeElement.vue
@@ -0,0 +1,18 @@
+<template>
+  <li>
+    <h3>{{ topicName }}</h3>
+    <p>{{ description }}</p>
+    <button @click="selectTopic(id)">Learn More</button>
+  </li>
+</template>
+
+<script>
+export default {
+  inject: {
+    selectTopic: {
+      from: 'selectTopic'
+    }
+  },
+  props: ['id', 'topicName', 'description']
+};
+</script>
---
+++ src/components/NewFriend.vue
@@ -0,0 +1,42 @@
+<template>
+  <form @submit.prevent="addNewFriend">
+    <div><label>Name<input v-model="name" type="text"></label></div>
+    <div><label>Email<input v-model="email" type="email"></label></div>
+    <div><label>Phone<input v-model="phone" type="tel"></label></div>
+    <div>
+      <button>Add New Friend</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "NewFriend",
+  emits: ['add-new-friend'],
+  data() {
+    return {
+      name: "",
+      email: "",
+      phone: ""
+    }
+  },
+  methods: {
+    addNewFriend() {
+      const friend = {
+        name: this.name,
+        email: this.email,
+        phone: this.phone
+      }
+      this.$emit('add-new-friend', friend)
+      this.name = ""
+      this.email = ""
+      this.phone = ""
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
