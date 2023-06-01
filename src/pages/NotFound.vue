89ce329203db3486531f8438904a403b4a383e1d
ViktarStar <v.starovoitou@belvg.com>
2023-06-01 20:36:47 +0200

    added styles for not found page

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,3 +1,8 @@
 <template>
-  <section>Not Found</section>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>This page couldn't be found - maybe check out all our <router-link to="/coaches">coaches</router-link></p>
+    </base-card>
+  </section>
 </template>
